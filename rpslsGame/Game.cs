c15835bcc39e02013efbcd3794b5d22c0c46f034
Andrew Curzon <andrew.w.curzon@gmail.com>
2020-02-11 14:37:38 -0600

    Reduced amount of errors user could create by improper inputs. Fleshed out first player choice further.

--- rpslsGame/Game.cs
+++ rpslsGame/Game.cs
@@ -13,10 +13,16 @@ namespace rpslsGame
         AIPlayer aiPlayer2;
         Gestures gestures;
         string input;
+        string choice1;
+        string choice2;
+        string aiChoice;
 
         public Game()
         {
             gestures = new Gestures();
+            humanPlayer1 = new HumanPlayer();
+            humanPlayer2 = new HumanPlayer();
+            aiPlayer2 = new AIPlayer();
         }
 
         public void GameSetUp()
@@ -35,20 +41,27 @@ namespace rpslsGame
 
             if(input == "1")
             {
-                humanPlayer1 = new HumanPlayer();
-                aiPlayer2 = new AIPlayer();
-
                 Console.WriteLine("Enter player one's name: ");
                 humanPlayer1.name = Console.ReadLine();
+                while (humanPlayer1.name == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("You didn't enter anything. Try again.");
+                    Console.WriteLine("\nEnter player one's name: ");
+                    humanPlayer1.name = Console.ReadLine();
+                }
                 Console.WriteLine("\nName your opponent (like in Pokemon): ");
                 aiPlayer2.name = Console.ReadLine();
-
+                while(aiPlayer2.name == "")
+                {
+                    Console.Clear();
+                    Console.WriteLine("You didn't enter anything. Try again.");
+                    Console.WriteLine("\nName your opponent (like in Pokemon): ");
+                    aiPlayer2.name = Console.ReadLine();
+                }
             }
             else if(input == "2")
             {
-                humanPlayer1 = new HumanPlayer();
-                humanPlayer2 = new HumanPlayer();
-
                 Console.WriteLine("Enter player one's name: ");
                 humanPlayer1.name = Console.ReadLine();
                 Console.WriteLine("\nEnter player two's name: ");
@@ -59,18 +72,46 @@ namespace rpslsGame
         public void MakeChoicePlayer1()
         {
             Console.Clear();
-            Console.WriteLine("Okay, " + humanPlayer1.name + ", choose your gesture!");
+            Console.WriteLine("Okay, " + humanPlayer1.name + ", choose your gesture! [Pick by typing the corresponding number on the left.]");
             for (int i = 0; i < gestures.gestures.Count; i++)
             {
                 Console.WriteLine(i + ".) " + gestures.gestures[i]);
             }
-            string choice = Console.ReadLine();
+            string choice1 = Console.ReadLine();
+            while (choice1 != "0" && choice1 != "1" && choice1 != "2" && choice1 != "3" && choice1 != "4")
+            {
+                Console.WriteLine("You did choose a correct option. Try again.");
+                choice1 = Console.ReadLine();
+            }
+
+        }
+
+        public void MakeChoicePlayer2()
+        {
+            Console.Clear();
+             Console.WriteLine("Okay, " + humanPlayer2.name + ", choose your gesture!");
+            for (int i = 0; i < gestures.gestures.Count; i++)
+            {
+                Console.WriteLine(i + ".) " + gestures.gestures[i]);
+            }
+            string choice2 = Console.ReadLine();
+        }
+
+        public void MakeChoiceAI()
+        {
+            Console.Clear();
+            Console.WriteLine(aiPlayer2.name + " chooses his gesture at random...");
+            Random rndGest = new Random();
+            int intRndGest = Convert.ToInt32(rndGest.Next(0, 4));
+            Console.WriteLine(aiPlayer2.name + " chooses " + gestures.gestures[intRndGest]);
         }
 
         public void Initialize()
         {
             GameSetUp();
             MakeChoicePlayer1();
+            MakeChoicePlayer2();
+            MakeChoiceAI();
         }
     }
 }
